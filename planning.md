43b148f11e75601efbeed6094c1c709b452cba1f
geici <geicianecosta43@gmail.com>
2022-11-03 21:41:34 -0400

    used equal isntead of sctrily equal to compare the answer with the data coming from the api

--- planning.md
+++ planning.md
@@ -3,13 +3,13 @@
 
 <ul> 
 
-<li>Planning started on cleaning up the app and deleting unecessary files.</li>
+<li>I started on cleaning up the app and deleting unecessary files.</li>
 <br>
 <li>Second I reset my CSS to reduce browser inconsistencies (border, margin, font of size headings and etc).</li>
 <br>
 <li>After that, utilizing my teacher knowledge on second grader students (their developmental stages and what they are able to do at this age). I then started thinking about what features this app needs to provide a great user experience. Next comes the knowledge on UX that I aquired trough courses, practice and what I do daily at work while building Investor Relations Websites.</li>
 
-<li>I know The app will need to be fun, colorful, and utilizing icons and images to make it atractive to students. </li>
+<li>I know The app would need to be fun, colorful, and the main way of communicating with the user would have to be trougt icons and images. </li>
 <br>
 <li>The next part will be the component tree and how to pass data between components. I will utilize a landing page that will have buttons for: entering the game or entering the score page that I will possibly build in the future. React Router will be utilized for switching between components. </li>
 <br>
@@ -17,10 +17,23 @@
 <li> On the next step, I went ahead I checked the documentation for the Math Api, it seems fairly simple and fun to use. After reading their documentation, I realized that the parameters for URL (the one we will utilize for the API call) will be encoded, so after the = sign will come our first random number, then "%2B" represents this character "+" and then we will put our last number in the end of the URL. I plan on using template literals for making the API call, template literals make it a lot easier to insert variables into the URL.</li>
 <br>
 
-<li>I will create a component called "form", which will hold: both random numbers for adding the numbers, and the field for the student to put their answer. </li>
 
-<br>
-<li>We need to create a state for both of the random numbers, the student's answer and their score (this will be implemented only if I have time). Utilizing the Usestate Hook, we will generate two random numbers that will make an API call to API Math. I like utilizing axios to make requests because it is shorter than fetch and produces more readable code.</li>
+## Component Tree and logic
+
+<p>We the App component currently organized our routes and components: the HEADER and Footer components will always show on any component. The Landing page will be the first one to appear when the page first loads, then the user can choose if they want to view the high scores or if they want to plah the game. </p>
+
+<p>The form component is the main component in this game, from that app we make the Api Call and give the user the feedback wether it is correct or not. </p>
+
+
+## Form Component and Detailed logic
+
+<p>To manage state in this application I used Use state and Use effect hooks, I also utilized Axios to make the API call cleaner. </p>
+<ol>
+<li>For the random numbers that will be generated, and added, I created two variables (number1 and number2) and utilized Math.Random (to generate random numbers) and Math.Floor (to round down number to the nearest integer). I also put those two numbers inside of the useEffect Hook, and added them to the dependency array. This way, whenever Javascript generates the random numbers, useEffect will take care of the state and make sure it is not out of pace with the User  Interface.</li> <br>
+
+<li>Inside of the same useEffect hook, I placed the Api call with axuis</li>
+ </ol>
+
  
 
 
--- src/components/form/Form.js
+++ src/components/form/Form.js
@@ -3,17 +3,24 @@ import axios from "axios";
 import "./form.css";
 import plus from "../../assets/images/plus.png";
 import equal from "../../assets/images/equal.png";
+import submit from "../../assets/images/submit.png";
 const Form = () => {
     const [number1, setNumber1] = useState(Math.floor(Math.random() * 50));
     const [number2, setNumber2] = useState(Math.floor(Math.random() * 100));
     const [answer, setAnswer] = useState("");
     const [data, setData] = useState([]);
-    const [numbertoCheck, setNumber] = useState(0)
+    const [correct, setCorrect] = useState("");
+    const [feedback, setFeedback] = useState(false);
+    const [score, setScore] = useState(0)
+    const [scoreArr, setScorearr] = useState([])
 
-    // const randomColor = Math.floor(Math.random() * 16777215).toString(16);
-    // console.log(number1 + number2)
-
-    useEffect(() => { }, [number1, number2]);
+    useEffect(() => {
+        axios
+            .get(`http://api.mathjs.org/v4/?expr=${number1}%2B${number2}`)
+            .then((res) => {
+                setData(res.data);
+            });
+    }, [number1, number2, data]);
 
     const handleChange = (e) => {
         setAnswer(e.target.value);
@@ -21,51 +28,63 @@ const Form = () => {
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        // setAnswer(e.target.value);
-       setNumber(answer)
-        getApiresult();
+        checkAnswer();
     };
 
     console.log(number1 + number2);
-    const getApiresult = () => {
-        axios
-            .get(`http://api.mathjs.org/v4/?expr=${number1}%2B${number2}`)
-            .then((res) => {
-                setData(res.data);
-            })
-            .catch(console.error);
-        checkAnswer()
-    };
-
 
     const checkAnswer = () => {
-
-        if (parseInt(numbertoCheck) ===data) {
-            console.log("equal");
-            console.log(typeof answer)
+        if (data) {
+            if (answer == data) {
+                setCorrect("Correct");
+                setFeedback("That's it! Great Job! 🥳");
+                setScore(score + 5)
+            }
+            if (answer != data) {
+                setCorrect("incorrect");
+                setFeedback("Oops, that does not look right 😔");
+            }
         }
-        if ( (parseInt(numbertoCheck) !==data) ){
-            console.log('not equal')
-            
-        }
-       
+        saveScores()
     };
-    // console.log(data);
-    console.log(typeof numbertoCheck)
+
+    console.log(score)
+
+    const saveScores = () => {
+
+        let sc = score;
+        console.log(sc + 2)
+        let arr = []
+        if (localStorage.getItem("score")) {
+            arr = JSON.parse(localStorage.getItem("score"));
+        }
+
+        if (sc > 0) {
+            arr.push(sc);
+        }
+
+
+        localStorage.setItem("score", JSON.stringify(arr));
+        setScorearr(arr)
+    }
+
+
     return (
         <div className="form_outer_container">
-            <section className="form_container">
+            <section className={`form_container ${correct}`}>
                 <form onSubmit={handleSubmit}>
                     <span className=" numbers number1">{number1}</span>
 
                     <img width="65px" src={plus} alt="plus sign" />
                     <span className="numbers number2">{number2}</span>
                     <img width="65px" src={equal} alt="equal sign" />
-                    <input value={answer} onChange={handleChange} type="text" />
+                    <input value={answer} onChange={handleChange} type="number" />
                     <button type="submit" className="comic sendButton">
                         SEND
+                        <img width="25px" src={submit} alt="submit icon" />
                     </button>
                 </form>
+                <h3>{feedback && feedback}</h3>
             </section>
         </div>
     );
